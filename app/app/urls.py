2f0bf50e59a5dd9fea0e21961513cf5891e18e5c
Ikarissu <razercool12@gmail.com>
2025-03-11 18:41:08 -0400

    modulo de probar conexion a bd+inventory con un unico view hola mundo

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('inventory.urls')),  # Incluye las URLs de tu aplicación
+    path('', include('prueba.urls')),  # Incluye las URLs de tu aplicación
+    path("inventory/", include("core.inventory.urls")),
 ]
