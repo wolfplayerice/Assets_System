7e6f4c58fa8f98b91010229b2c325c92479198e7
Ikarissu <razercool12@gmail.com>
2025-03-17 16:07:49 -0400

    pepe

--- app/inventory/forms.py
+++ app/inventory/forms.py
@@ -4,8 +4,8 @@ from .models import ACTIVE_CHOICES, Category, Brand
 class AssetCreate(forms.Form):
     model = forms.CharField(label="Modelo del equipo", max_length=255)
     serial_number = forms.CharField(label="Número de serie", max_length=255)
-    prefix = forms.ChoiceField(choices=[('BBVA-', 'BBVA'), ('BE ILE-', 'BE ILE')], label="Prefijo")
-    state_asset = forms.CharField(label="BBVA/BE ILE", max_length=255)
+    prefix = forms.ChoiceField(choices=[('BBVA-', 'BBVA'), ('BE-', 'BE')], label="Prefijo")
+    state_asset = forms.CharField(label="BBVA/BE", max_length=255)
     fk_category = forms.ModelChoiceField(queryset=Category.objects.all(), label="Categoría",)
     fk_brand = forms.ModelChoiceField(queryset=Brand.objects.all(), label="Marca",)
     status = forms.ChoiceField(label='Estado del bien',choices=ACTIVE_CHOICES, widget=forms.Select(attrs={'id': 'status_select'}))
