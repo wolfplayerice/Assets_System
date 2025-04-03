c36fde5c7addc99fd01df0e953366481f279f69b
wolfplayerice <kgandres13@gmail.com>
2025-04-03 13:02:17 -0400

    estetica y dashboard

--- app/home/views.py
+++ app/home/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from inventory.models import Asset
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from brand.models import Brand
+from category.models import Category
 
 @login_required
 def home(request):
+
     return render(request, 'home.html', {
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
@@ -13,6 +16,8 @@ def home(request):
 @login_required
 def dashboard(request):
     # Conteo de Dispositivos operativos y no operativos, bienes y usuarios
+    categories = Category.objects.all()
+    brands = Brand.objects.all()
     total_assets = Asset.objects.count()
     total_user = User.objects.count()
     operational_assets = Asset.objects.filter(status=1).count()
@@ -26,5 +31,7 @@ def dashboard(request):
             # Conteo de Dispositivos operativos y no operativos, bienes y usuarios V.P
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
-        'success': success_message
+        'success': success_message,
+        'categories': categories,
+        'brands': brands,
     })
