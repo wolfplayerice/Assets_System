5a61b4e9b1d6e39a6568d723e246df5d38716609
Daniela Jr <danielaalexandrarangel@gmail.com>
2025-04-09 08:24:35 -0400

    e

--- app/app/settings.py
+++ app/app/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inventory',
-        'USER': 'nuser',
-        'PASSWORD': '456',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
