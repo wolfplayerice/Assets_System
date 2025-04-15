fd244bae7e12e900c6b311a70b000d00d1610790
Victor P <deerpg17x@gmail.com>
2025-04-15 11:57:26 -0400

    Ventana manual funcional, falta algo de estetica VP

--- app/manuals/views.py
+++ app/manuals/views.py
@@ -3,6 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def manuals(request):
-    return render(request, 'manuals/manuals.html', {
+    return render(request, 'manuals.html', {
         'message': 'Hola'
     })
